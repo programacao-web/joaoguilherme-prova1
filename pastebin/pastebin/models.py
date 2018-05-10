038069706ba72e563d6d6cd867a98643ded436ad
João Guilherme <joaaogui@gmail.com>
2018-05-10 18:28:19 -0300

    Comitão, questoes 1,2,3,4,5

--- pastebin/pastebin/models.py
+++ pastebin/pastebin/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Paste(models.Model):
+    title = models.CharField(max_length=50)
+    language = models.CharField(max_length=20)
+    content = models.TextField()
--- pastebin/pastebin/views.py
+++ pastebin/pastebin/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-
+from .models import Paste
+from .forms import PasteForm
 
 def index(request):
-    ctx = {}
+    parameters = PasteForm()
+    ctx = {'parameters':parameters}
     return render(request, 'pastebin/index.jinja2', ctx)
 
 
@@ -14,3 +16,12 @@ def paste(request, id):
 def language_list(request, language):
     ctx = {'pastes': []}
     return render(request, 'pastebin/paste-language.jinja2', ctx)
+
+def paste_new(request):
+    paste = Paste.objects.create(
+        title = request.POST.get('title'),
+        language = request.POST.get('language'),
+        content = request.POST.get('content')
+    )
+    ctx = {'paste':paste}
+    return render(request, 'pastebin/paste-detail.jinja2', ctx)
